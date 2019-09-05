f164e29b838a56398ff2101114813a2acb00d3e9
uldisn <uldisnelsons@gmail.com>
2019-09-05 11:33:38 +0300

    To TableTdCell added getHtml()

--- TableTdCell.php
+++ TableTdCell.php
@@ -3,6 +3,8 @@
 namespace moonland\phpexcel;
 
 
+use yii\helpers\Html;
+
 class TableTdCell
 {
     public $tag = 'td';
@@ -16,19 +18,21 @@ class TableTdCell
     public $numberDecimals = false;
     public $columnAutoSize = true;
 
+    /** @var string */
+    public $tooltipText;
+
     public $style = [];
 
     public $params;
 
     /**
      * TableCell constructor.
-     * @param string $value*
+     * @param string $value *
      * @param array $class
      * @param $colspan
      * @param $rowspan
-
      */
-    public function __construct(string $value = '', array $class =[], $colspan = 1,  $rowspan = 1)
+    public function __construct(string $value = '', array $class = [], $colspan = 1, $rowspan = 1)
     {
         $this->value = $value;
         $this->colspan = $colspan;
@@ -36,23 +40,55 @@ class TableTdCell
         $this->class = $class;
     }
 
-    public function setParam(string $name, $value)
+    public function setParam(string $name, $value): void
     {
         $this->params[$name] = $value;
     }
 
 
-    public function getParam(string $name)
+    public function getParam(string $name): bool
     {
-        return $this->params[$name]??false;
+        return $this->params[$name] ?? false;
     }
 
-    public function getValue()
+    public function getValue(): string
     {
-        if($this->numberDecimals !== false) {
-            return number_format(round($this->value, $this->numberDecimals), $this->numberDecimals,'.','');
+        if ($this->numberDecimals !== false) {
+            return number_format(round($this->value, $this->numberDecimals), $this->numberDecimals, '.', '');
         }
         return $this->value;
     }
+
+    public function getHtml(): string
+    {
+        $options = [];
+        if ($this->colspan !== 1) {
+            $options['colspan'] = $this->colspan;
+        }
+        if ($this->rowspan !== 1) {
+            $options['rowspan'] = $this->rowspan;
+        }
+        if ($this->class) {
+            $options['class'] = $this->class;
+        }
+        if ($this->nowrap) {
+            $options['class'] = array_merge($this->class, ['text-nowrap']);
+        }
+
+        if ($this->tooltipText) {
+            $options['data-title'] = $this->tooltipText;
+            $options['data-placement'] = 'top';
+            $options['data-toggle'] = 'tooltip';
+            $options['data-container'] = 'body';
+            $options['data-original-title'] = '';
+        }
+
+        if(!$this->url) {
+            return Html::tag($this->tag, $this->getValue(), $options);
+        }
+
+        return Html::tag($this->tag, Html::a($this->getValue(), $this->url, $this->urlOptions), $options);
+    }
+
 
 }
